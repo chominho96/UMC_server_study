73e2a47b0902a0ce10301cc2107ff41282255c2d
CHO Min Ho <chominho96@inha.edu>
2022-05-04 20:45:59 +0900

    Add Project Files
    1. add project files

---
+++ umc_server_study/src/test/java/com/umc/umc_server_study/UmcServerStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umc.umc_server_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmcServerStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
